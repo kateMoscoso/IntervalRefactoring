59409f8a9e8d238cc6f46912867af53b6591a4f9
Kate <katherin.mleon@alumnos.upm.es>
2015-04-03 19:33:19 +0200

    LeftOpened - se añade el método boolean includes(Interval interval) y su implementacion

--- src/intervals/LeftOpened.java
+++ src/intervals/LeftOpened.java
@@ -9,4 +9,25 @@ public class LeftOpened extends Interval{
 	public boolean includes(double value) {
 		return this.getMinimum() < value && value <= this.getMaximum();
 	}
+	public boolean includes(Interval interval) {
+		boolean minimumIncluded = this.includes(interval.getMinimum());
+		boolean maximumIncluded = this.includes(interval.getMaximum());
+		switch (interval.getOpening()) {
+		case BOTH_OPENED:
+			return (minimumIncluded || this.getMinimum() == interval.getMinimum())
+					&& (maximumIncluded || this.getMaximum() == interval.getMaximum());
+		case LEFT_OPENED:
+			return (minimumIncluded || this.getMinimum() == interval.getMinimum())
+					&& (maximumIncluded || this.getMaximum() == interval.getMaximum());
+		case RIGHT_OPENED:
+			return (minimumIncluded)
+					&& (maximumIncluded || this.getMaximum() == interval.getMaximum());
+		case UNOPENED:
+			return (minimumIncluded)
+					&& (maximumIncluded || this.getMaximum() == interval.getMaximum());
+		default:
+			assert false;
+			return false;
+		}
+	}
 }
