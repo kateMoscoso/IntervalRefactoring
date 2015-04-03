3f01f5b31f5d4498f494423342df3702a626fe4c
Kate <katherin.mleon@alumnos.upm.es>
2015-04-03 19:29:57 +0200

    BothOpened - se añade el método boolean includes(Interval interval) y su implementacion

--- src/intervals/BothOpened.java
+++ src/intervals/BothOpened.java
@@ -12,5 +12,25 @@ public class BothOpened extends Interval{
 	public boolean includes(double value) {
 		return this.getMinimum() < value && value < this.getMaximum();
 	}
+	public boolean includes(Interval interval) {
+		boolean minimumIncluded = this.includes(interval.getMinimum());
+		boolean maximumIncluded = this.includes(interval.getMaximum());
+		switch (interval.getOpening()) {
+		case BOTH_OPENED:
+			return (minimumIncluded || this.getMinimum() == interval.getMinimum())
+					&& (maximumIncluded || this.getMaximum() == interval.getMaximum());
+		case LEFT_OPENED:
+			return (minimumIncluded || this.getMinimum() == interval.getMinimum())
+					&& (maximumIncluded);
+		case RIGHT_OPENED:
+			return (minimumIncluded)
+					&& (maximumIncluded || this.getMaximum() == interval.getMaximum());
+		case UNOPENED:
+			return (minimumIncluded) && (maximumIncluded);
+		default:
+			assert false;
+			return false;
+		}
+	}
 
 }
