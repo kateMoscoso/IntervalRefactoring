d62bf53d538bb92f57e1691cdf4a1642bacecd9f
Kate <katherin.mleon@alumnos.upm.es>
2015-04-03 19:45:03 +0200

    UnOpened - se añade el método boolean includes(Interval interval) y su implementacion

--- src/intervals/UnOpened.java
+++ src/intervals/UnOpened.java
@@ -9,4 +9,26 @@ public class UnOpened extends Interval {
 	public boolean includes(double value) {
 		return this.getMinimum() <= value && value <= this.getMaximum();
 	}
+	public boolean includes(Interval interval) {
+		boolean minimumIncluded = this.includes(interval.getMinimum());
+		boolean maximumIncluded = this.includes(interval.getMaximum());
+		switch (interval.getOpening()) {
+		case BOTH_OPENED:
+			return (minimumIncluded || this.getMinimum() == interval.getMinimum())
+					&& (maximumIncluded || this.getMaximum() == interval.getMaximum());
+		case LEFT_OPENED:
+			return (minimumIncluded || this.getMinimum() == interval.getMinimum())
+					&& (maximumIncluded || this.getMaximum() == interval.getMaximum());
+		case RIGHT_OPENED:
+			return (minimumIncluded || this.getMinimum() == interval.getMinimum())
+					&& (maximumIncluded || getMaximum() == interval.getMaximum());
+		case UNOPENED:
+			return (minimumIncluded || this.getMinimum() == interval.getMinimum())
+					&& (maximumIncluded || this.getMaximum() == interval.getMaximum());
+		default:
+			assert false;
+			return false;
+		}
+	}
 }
+
