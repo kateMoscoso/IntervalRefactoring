69873b2b5963d1c9e9b8a318808930fbdc3ca5b6
Kate <katherin.mleon@alumnos.upm.es>
2015-04-03 19:10:34 +0200

    UnOpened - añadimos el extends a la clase intervalo, el constructor y modifcamos la operacion boolean intervals(double value)

--- src/intervals/UnOpened.java
+++ src/intervals/UnOpened.java
@@ -1,7 +1,12 @@
 package intervals;
 
-public class UnOpened {
+public class UnOpened extends Interval {
+	public UnOpened(double minimum, double maximum, Opening opening) {
+		super(minimum, maximum, opening);
+		// TODO Auto-generated constructor stub
+	}
+
 	public boolean includes(double value) {
-		return minimum <= value && value <= maximum;
+		return this.getMinimum() <= value && value <= this.getMaximum();
 	}
 }
