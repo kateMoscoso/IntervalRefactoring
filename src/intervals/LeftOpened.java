24236edb15e42ba46ed45c792fbfa7a41f031510
Kate <katherin.mleon@alumnos.upm.es>
2015-04-03 19:08:44 +0200

    LeftOpened - añadimos el extends a la clase intervalo, el constructor y modifcamos la operacion boolean intervals(double value)

--- src/intervals/LeftOpened.java
+++ src/intervals/LeftOpened.java
@@ -1,7 +1,12 @@
 package intervals;
 
-public class LeftOpened {
+public class LeftOpened extends Interval{
+	public LeftOpened(double minimum, double maximum, Opening opening) {
+		super(minimum, maximum, opening);
+		// TODO Auto-generated constructor stub
+	}
+
 	public boolean includes(double value) {
-		return minimum < value && value <= maximum;
+		return this.getMinimum() < value && value <= this.getMaximum();
 	}
 }
