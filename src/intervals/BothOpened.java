6ac867d00a085c13cc61e767afa0d2747f75aa11
Kate <katherin.mleon@alumnos.upm.es>
2015-04-03 19:07:12 +0200

    añadimos el extends a la clase intervalo, el constructor y modifcamos la operacion booelan intervals(double value)

--- src/intervals/BothOpened.java
+++ src/intervals/BothOpened.java
@@ -1,17 +1,16 @@
 package intervals;
 
-public class BothOpened {
-	private double minimum;
-	private double maximum;
-	private Opening opening;
-	public boolean includes(double value) {
-		return minimum < value && value < maximum;
+public class BothOpened extends Interval{
+	
+
+
+	public BothOpened(double minimum, double maximum, Opening opening) {
+		super(minimum, maximum, opening);
 	}
-	public boolean includes(Interval interval) {
-		boolean minimumIncluded = this.includes(interval.minimum);
-		boolean maximumIncluded = this.includes(interval.maximum);
-		return (minimumIncluded || minimum == interval.minimum)
-				&& (maximumIncluded || maximum == interval.maximum);
+
+	@Override
+	public boolean includes(double value) {
+		return this.getMinimum() < value && value < this.getMaximum();
 	}
 
 }
