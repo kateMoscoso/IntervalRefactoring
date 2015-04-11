6a2b48eea776ad2b77ed86d33fc4b85062421c96
bl0804 <bl0804@192.168.7.15>
2015-04-11 12:16:58 +0200

    Se realizan los cambios para que en el contructor de LeftOpened se declare el opening a el valor adecuado y se implementa el getOpening

--- src/intervals/LeftOpened.java
+++ src/intervals/LeftOpened.java
@@ -1,8 +1,10 @@
 package intervals;
 
 public class LeftOpened extends Interval{
-	public LeftOpened(double minimum, double maximum, Opening opening) {
-		super(minimum, maximum, opening);
+	private Opening opening;
+	public LeftOpened(double minimum, double maximum) {
+		super(minimum, maximum);
+		this.opening = Opening.LEFT_OPENED;
 		// TODO Auto-generated constructor stub
 	}
 
@@ -59,5 +61,11 @@ public class LeftOpened extends Interval{
 	public boolean includes(UnOpened interval) {
 		return (this.includes(interval.getMinimum()))
 				&& (this.includes(interval.getMaximum()) || this.getMaximum() == interval.getMaximum());
+	}
+
+	@Override
+	public Opening getOpening() {
+		// TODO Auto-generated method stub
+		return opening;
 	}
 }
