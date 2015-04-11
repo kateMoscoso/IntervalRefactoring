c21021beab881b23f2c0695f1e2516a87490b258
bl0804 <bl0804@192.168.7.15>
2015-04-11 12:19:50 +0200

    Se realizan los cambios para que en el contructor de UnOpened se declare el opening a el valor adecuado y se implementa el getOpening

--- src/intervals/UnOpened.java
+++ src/intervals/UnOpened.java
@@ -1,8 +1,10 @@
 package intervals;
 
 public class UnOpened extends Interval {
-	public UnOpened(double minimum, double maximum, Opening opening) {
-		super(minimum, maximum, opening);
+	private Opening opening;
+	public UnOpened(double minimum, double maximum) {
+		super(minimum, maximum);
+		this.opening = Opening.UNOPENED;
 		// TODO Auto-generated constructor stub
 	}
 
@@ -59,6 +61,12 @@ public class UnOpened extends Interval {
 	public boolean includes(UnOpened interval) {
 		return (this.includes( interval.getMinimum()) || this.getMinimum() ==  interval.getMinimum())
 				&& this.includes( interval.getMaximum()) || this.getMaximum() ==  interval.getMaximum();
+	}
+
+	@Override
+	public Opening getOpening() {
+		// TODO Auto-generated method stub
+		return this.opening;
 	}
 }
 
