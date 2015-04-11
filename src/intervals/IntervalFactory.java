1c20ea3afce608a2fa448760547cf2e325fbc2c5
bl0804 <bl0804@192.168.7.15>
2015-04-11 12:20:30 +0200

    Se modifica el switch correspodiente a la nueva implementacion de la clase Interval

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -6,13 +6,13 @@ public class IntervalFactory {
 	public static Interval getInterval(double minimum, double maximum, Opening opening) {
 		switch (opening) {
 		case BOTH_OPENED:
-			return new BothOpened(minimum, maximum, opening);
+			return new BothOpened(minimum, maximum);
 		case LEFT_OPENED:
-			return new LeftOpened(minimum, maximum, opening);
+			return new LeftOpened(minimum, maximum);
 		case RIGHT_OPENED:
-			return new RightOpened(minimum, maximum, opening);
+			return new RightOpened(minimum, maximum);
 		case UNOPENED:
-			return new UnOpened(minimum, maximum, opening);
+			return new UnOpened(minimum, maximum);
 		default:
 			return null;
 		}
