ddb4eb2ae3d666ceea946b6690e01a7ee1c9a26f
katherin <katherin@Kate>
2015-04-13 19:27:38 +0200

    implementamos la primera version de la clase punto

--- src/intervals/Punto.java
+++ src/intervals/Punto.java
@@ -1,5 +1,18 @@
 package intervals;
 
 public class Punto {
+	private double valor;
+	
+	public Punto (double valor){
+		this.valor = valor;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
 
 }
